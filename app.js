5b99bcbfd269d0e347fd44b3fa39ce04556efbe5
Ginolechuga <gino_lechuga@hotmail.com>
2025-04-21 18:29:17 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const supabaseUrl = 'https://fysgndnzudqhcfzmjqxn.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5c2duZG56dWRxaGNmem1qcXhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyNzgwNTksImV4cCI6MjA2MDg1NDA1OX0.aE5okasKS5byVVDzRtRUhBxncIlpf00QG7H8HL8Jm7s'
+const supabaseKey = ''
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 document.getElementById('form').addEventListener('submit', async (e) => {
   e.preventDefault()
-  
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5c2duZG56dWRxaGNmem1qcXhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyNzgwNTksImV4cCI6MjA2MDg1NDA1OX0.aE5okasKS5byVVDzRtRUhBxncIlpf00QG7H8HL8Jm7s
   const nombre = document.getElementById('nombre').value
   const correo = document.getElementById('correo').value
 
